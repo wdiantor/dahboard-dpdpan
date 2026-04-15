29e72de1ec487443c68f0f6f7a73aaec044025a2
wdiantor <wdiantor@gmail.com>
2026-04-15 18:17:17 +0700

    login

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function login() {
+let user = document.getElementById("username").value;
+let pass = document.getElementById("password").value;
+
+if (user === "admin" && pass === "12345") {
+window.location.href = "dashboard.html";
+} else {
+alert("Login gagal!");
+}
+}
 
