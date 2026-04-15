545ddf274b9f5cd337fc2fd329cdd96b62e87de5
wdiantor <wdiantor@gmail.com>
2026-04-15 18:20:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,30 @@ window.location.href = "dashboard.html";
 alert("Login gagal!");
 }
 }
+fetch("LINK_CSV_KAMU")
+.then(res => res.text())
+.then(data => {
+let rows = data.split("\n").slice(1);
 
+```
+let table = document.querySelector("#dataTable tbody");
+
+rows.forEach(row => {
+  let cols = row.split(",");
+  let tr = document.createElement("tr");
+
+  cols.forEach(col => {
+    let td = document.createElement("td");
+    td.innerText = col;
+    tr.appendChild(td);
+  });
+
+  table.appendChild(tr);
+});
+```
+
+});
+
+function logout() {
+window.location.href = "index.html";
+}
